231d7d706bfa5aeb3fd7fccd81bc8ed8d36b0018
m.kumeta <kumeta1202@gmail.com>
2023-11-01 15:37:52 +0900

    compare structure

--- app/langsmith_tutorial/compare_structure.py
+++ app/langsmith_tutorial/compare_structure.py
@@ -96,18 +96,90 @@ def return_source_test(text: str) -> None:
     }
     answer = {
         "answer": inputs | prompt | model | StrOutputParser(),
-        "docs": itemgetter("docs"),
+        "docs": itemgetter("docs") | RunnableLambda(_combine_source_documents),
     }
 
     chain = RunnablePassthrough() | retrieved_documents | answer
     inputs = {"question": text}
     result = chain.invoke(inputs)
+    # docs = _combine_source_documents(result['docs'])
     print(result)
+    print(result['docs'])
 
+
+def _combine_source_documents(documents: list) -> str:
+    docs = [doc.page_content for doc in documents]
+    docs = "\n\n".join(docs)
+    return docs
+
+
+def custom_chain_type(text: str) -> None:
+    vectorstore = get_vectorstore()
+    # retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
+    retriever = vectorstore.as_retriever()
+    model = ChatOpenAI(temperature=0)
+
+    template = """以下のcontextのみに基づいて質問に答えて下さい。
+    {context}
+
+    質問: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+    chain_type_kwargs = {"prompt": prompt}
+    qa = RetrievalQA.from_chain_type(llm=model, chain_type="refine", retriever=retriever,
+                                     chain_type_kwargs=chain_type_kwargs)
+
+    inputs = {
+        "context": itemgetter("docs"),
+        "question": itemgetter("question")
+    }
+
+    retrieved_documents = {
+        "question": RunnablePassthrough(),
+        "docs": itemgetter("question") | retriever,
+    }
+
+    answer = {
+        "answer": inputs | prompt | model | StrOutputParser(),
+        "docs": itemgetter("docs") | RunnableLambda(_combine_source_documents),
+    }
+
+    chain = RunnablePassthrough() | retrieved_documents | answer
+    inputs = {"question": text}
+    result = chain.invoke(inputs)
+    # docs = _combine_source_documents(result['docs'])
+    print(result)
+    print(result['docs'])
+
+
+def custom_chain_type_(text: str) -> str:
+    vectorstore = get_vectorstore()
+    retriever = vectorstore.as_retriever()
+    template = """以下のcontextのみに基づいて質問に答えて下さい。
+    {context}
+
+    質問: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+    chain_type_kwargs = {"prompt": prompt}
+    chain = (
+            RetrievalQA.from_chain_type(
+                llm=ChatOpenAI(temperature=0),
+                chain_type="stuff",  # or any other appropriate chain type
+                retriever=retriever,
+                return_source_documents=True,
+                chain_type_kwargs=chain_type_kwargs
+            )
+            | RunnablePassthrough()
+            | StrOutputParser()
+    )
+    result = chain.invoke({"query": text})
+    return result
 
 if __name__ == '__main__':
     question = "キングオブコントに優勝したのは誰ですか？"
     # question = "ジャニーズ事務所の次の名前は？"
     # print(default_news_role(question))
     # print(new_news_role(question))
-    return_source_test(question)
+    # return_source_test(question)
+    print(custom_chain_type_(question))
